201a6aef4a981588617da9b37f692736474e12a4
Piret Saar <piret.saar.003@khk.ee>
2022-01-29 14:38:23 +0200

    templating directory setup

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const path = require('path')
 const app = express();
 // add public direction
 app.use(express.static('public'));
+// add views directory path
+app.set('views', path.join(__dirname, 'views'));
+// add views template engine
+app.set('view engine', 'ejs');
 
 app.get('/user/:username', (req, res)=> {
     // get parameter data from address row
     let user = req.params.username;
     // use this data in template
-    res.render('index.ejs', {username : user});
+    res.render('index', {username : user});
 });
 
 app.listen(3000, ()=> {
