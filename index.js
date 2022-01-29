d94c88491146ac5c04f26febf1e1c6d36e8cca85
Piret Saar <piret.saar.003@khk.ee>
2022-01-29 14:13:38 +0200

    Basic use of ejs

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require('express')
 const app = express();
 
-//set up brouther address row router
-app.get('/', (req, res)=> {
-    //create html response
-    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About Us</a>');
+app.get('/user/:username', (req, res)=> {
+    // get parameter data from address row
+    let user = req.params.username;
+    // use this data in template
+    res.render('index.ejs', {username : user});
 });
 
-app.get('/contact', (req, res)=> {
-    res.send('<h1>Contact Us Page</h1>');
-})
-
-app.get('/about', (req, res)=> {
-    res.send('<h1>About Us Page</h1>');
-})
-
-app.get('*', (req, res)=> {
-    res.send('404. This page does not exist. <a href="/">Go to HomePage</a>');
-})
-
-// listen app via port
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000');
-})
+});
